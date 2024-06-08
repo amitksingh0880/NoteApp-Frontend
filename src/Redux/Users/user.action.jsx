0f50e74c2e76e81c303835ea2b7362d63136cf78
amitksingh0880 <amitksingh.1204@gmail.com>
2024-06-08 16:08:44 +0530

    redux

--- src/Redux/Users/user.action.jsx
+++ src/Redux/Users/user.action.jsx
@@ -1,33 +1,28 @@
-import axios from "axios"
-import { LOGIN_USER_ERROR, LOGIN_USER_LOADING, LOGIN_USER_SUCCESS } from "./user.type"
+import axios from "axios";
+import { LOGIN_USER_ERROR, LOGIN_USER_LOADING, LOGIN_USER_SUCCESS } from "./user.type";
 import { BASE_URL } from "../../Constants/config";
 
-export const getUser=(email,password)=>async(dispatch)=>{
-
-    dispatch({type:LOGIN_USER_LOADING})
-    try {  
-        
-        let data = await axios.post(BASE_URL+"/user/login",{
-                  email : email,
-                  password: password,
+export const getUser = (email, password) => async (dispatch) => {
+    dispatch({ type: LOGIN_USER_LOADING });
+    try {
+        const data = await axios.post(BASE_URL + "/user/login", {
+            email: email,
+            password: password,
         });
-        let {message ,token, user , status} = data.data
+        const { message, token, user, status } = data.data;
         console.log(data);
         console.log(message);
-        if(status==1){
+        if (status === 1) {
             alert(message);
-            dispatch({ type:LOGIN_USER_SUCCESS, 
-                       payload:{token,user}
-                    })
-        }else{
-            alert(message)
-            dispatch({type:LOGIN_USER_ERROR})
+            dispatch({ 
+                type: LOGIN_USER_SUCCESS, 
+                payload: { token, user }
+            });
+        } else {
+            alert(message);
+            dispatch({ type: LOGIN_USER_ERROR });
         }
-
-      
-    } catch (error){
-        dispatch({type:LOGIN_USER_ERROR})
+    } catch (error) {
+        dispatch({ type: LOGIN_USER_ERROR });
     }
-  
-}   
-// export default getUser;
+};
--- src/Redux/Users/user.reducer.jsx
+++ src/Redux/Users/user.reducer.jsx
@@ -1,50 +1,41 @@
-import {
-  LOGIN_USER_ERROR,
-  LOGIN_USER_LOADING,
-  LOGIN_USER_SUCCESS,
-  LOGOUT,
-} from "./user.type";
+import { LOGIN_USER_ERROR, LOGIN_USER_LOADING, LOGIN_USER_SUCCESS, LOGOUT } from "./user.type";
 
 const initialState = {
-  name: null,
-  auth: false,
-  token: null,
-  loading: false,
-  error: false,
-  email: null
+    user: null,
+    auth: false,
+    token: null,
+    loading: false,
+    error: false,
 };
 
-const userReducer=(state = initialState, action)=> {
-  const { type, payload } = action;
-
-  switch (type) {
-    case LOGIN_USER_LOADING:
-      return {
-        ...state,
-        loading: true,
-      };
-    case LOGIN_USER_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        error: false,
-        token: payload.token,
-        auth: true,
-        name: payload.user.name,
-        email: payload.user.email
-      };
-    case LOGIN_USER_ERROR:
-      return {
-        ...state,
-        loading: false,
-        error: true,
-      };
-    case LOGOUT:{
-        return initialState
+const userReducer = (state = initialState, action) => {
+    const { type, payload } = action;
+    switch (type) {
+        case LOGIN_USER_LOADING:
+            return {
+                ...state,
+                loading: true,
+            };
+        case LOGIN_USER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: false,
+                token: payload.token,
+                auth: true,
+                user: payload.user.name,
+            };
+        case LOGIN_USER_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: true,
+            };
+        case LOGOUT:
+            return initialState;
+        default:
+            return state;
     }
-    default:
-      return state;
-  }
-}
+};
 
 export default userReducer;
